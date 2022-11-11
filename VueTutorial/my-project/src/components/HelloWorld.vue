66ad41d94578b0ee407d3d634c26175b4d4756ba
ganeshIIT <21f1002245@student.onlinedegree.iitm.ac.in>
2022-11-11 12:39:36 +0530

    -

---
+++ VueTutorial/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Your first Vue file!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
